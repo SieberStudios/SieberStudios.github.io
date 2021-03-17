3db712a0323f65e98ff11f19bffea833ab30b948
sieber2210 <jsieber53@gmail.com>
2021-03-17 04:01:23 -0400

    adding p5 libraries/working on new pages

--- sketch.js
+++ sketch.js
@@ -1,11 +1,25 @@
+let img;
+
+function preload(){
+    img = loadImage('images/Portfolio/Portfolio12.png');
+}
+
 function setup() {
-    let cnv = createCanvas(windowWidth,800);
-    cnv.position();
-  }
-  
-  function draw() {
-    background(255);
-    strokeWeight(5);
-    fill(100);
-    circle(width/2, height/2, 50);
-  }
+    createCanvas(windowWidth,windowHeight);
+    textSize(32);
+    textAlign(CENTER, CENTER);
+}
+
+function draw() {
+    //background(0);
+    fill(255);
+    text("Hello World!", 200, 100);
+    imageMode(CENTER);
+    image(img, width/2,height/2);
+}
+
+function windowResized(){
+    resizeCanvas(windowWidth, windowHeight);
+}
+
+// https://p5js.org/reference/#/p5/createVideo
