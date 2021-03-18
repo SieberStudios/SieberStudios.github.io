4ca64c09244ed7851aaa63e66265ef9325186033
sieber2210 <jsieber53@gmail.com>
2021-03-18 00:59:37 -0400

    Added video play to new page

--- sketch.js
+++ sketch.js
@@ -1,25 +1,31 @@
-let img;
-
-function preload(){
-    img = loadImage('images/Portfolio/Portfolio12.png');
-}
+let vid;
 
 function setup() {
-    createCanvas(windowWidth,windowHeight);
+    createCanvas(windowWidth, windowHeight);
+
+    vid = createVideo(['videos/ScenicSprintVid.mp4', 'ScenicSprintVid.wmv']);
+    vid.hide();
     fill(255);
-    textSize(32);
-    textAlign(CENTER,CENTER);
-    text("Hello World", 200, 100);
+    textSize(30);
 }
 
-function draw() {
-    //background(0);
-    imageMode(CENTER);
-    image(img, width/2,height/2);
+function draw(){
+    background(0);
+    text('Click me to play video', 750, 400);
+    image(vid, 300, 100, 1280, 720);
+}
+
+function mousePressed(){
+    vidLoad();
+}
+
+function vidLoad(){
+    vid.loop();
+    vid.volume(0);
 }
 
 function windowResized(){
+    window.location.reload();
     resizeCanvas(windowWidth, windowHeight);
+    vidLoad();
 }
-
-// https://p5js.org/reference/#/p5/createVideo
