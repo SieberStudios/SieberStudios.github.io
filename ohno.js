4845395ad36f78a09a51d98b3dd1cec63acb1a64
sieber2210 <jsieber53@gmail.com>
2021-04-05 19:32:44 -0400

    Updated styles / Added Oh No videos

--- ohno.js
+++ ohno.js
@@ -9,8 +9,8 @@ function setup() {
     c1 = color(0);
     setGradient(c1,c2);
 
-    var mp4 = 'videos/TunnelHeliVid.mp4';
-    var wmv = 'videos/TunnelHeliVid.wmv'
+    var mp4 = 'videos/OhNoVid.mp4';
+    var wmv = 'videos/OhNoVid.wmv'
 
     vid = createVideo([mp4, wmv]);
     vid.hide();
